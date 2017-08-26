f2b2ef3555004e6a61c680f898d4d0545c503785
komala <komaladeepthi@gmail.com>
2017-08-25 17:34:02 -0700

    Corrections made

--- js/app.js
+++ js/app.js
@@ -3,237 +3,238 @@ var map;
 var infowindow;
 var bounds;
 
-// List of Restaurants defined to appear on the page
-var initialSpaces = [
-          {"title": "Mika's Greek", "location": {"lat": 33.3495856 , "lng": -111.949818}, "fs_id": "515f0b677ab473662460166d" },
-          {"title": "Olive Garden", "location": {"lat": 33.34965, "lng": -111.9550835}, "fs_id": "4b5b5888f964a52068f628e3"},
-          {"title": "Tzikii Pita Grill", "location": {"lat": 33.3501265 , "lng": -111.9637567 }, "fs_id": "5151e5d9e4b03359ba73109f" },
-          {"title": "Crêpe Bar", "location": {"lat":33.3499291 , "lng":-111.9299471 }, "fs_id": "4fcedee30cd6e871c8030eed"},
-          {"title": "Garcia's Mexican Restaurant", "location": {"lat":33.3832233 , "lng":-111.9644216 }, "fs_id": "50d4f6ace4b028bbc68635c1"},
-          {"title": "Opa Life Greek Cafe", "location": {"lat":33.3777804 , "lng": -111.9360861 }, "fs_id": "53c84bcd498e35e3e45faab5"}
-        ];
+
 // Url elements for FourSquare API
 var BaseUrl = "https://api.foursquare.com/v2/venues/";
-    fsClient_id = "client_id=YMGR14U3YEE0DMAWI2BCVRMFG0UEJXITA4C5X22GMDFEDK55";
-    fsClient_secret = "&client_secret=DPTYGA0H2TCRKQQJ1UJKFHUSHNGZRPWIZVFRXADJCVYOKZP3";
-    fsVersion = "&v=20170101";
+var fsClient_id = "client_id=YMGR14U3YEE0DMAWI2BCVRMFG0UEJXITA4C5X22GMDFEDK55";
+var fsClient_secret = "&client_secret=DPTYGA0H2TCRKQQJ1UJKFHUSHNGZRPWIZVFRXADJCVYOKZP3";
+var fsVersion = "&v=20170101";
 
 // map initialisation function which gets called when the page is loaded
 function mapInit() {
-  "use strict";
-
-  // Custom marker image
-  var image = {
-    "url": "img/pizza.png"
-  };
+    "use strict";
+      // to hide error-section
+    var $mape = $(".map-error");
+    $mape.hide();
+      // Custom marker image
+    var image = {
+        "url": "img/pizza.png"
+    };
 
   // setting map options
-  var mapOptions = {
-    "center": {
-      "lat": 33.4255104,
-      "lng": -111.9400054
-    },
-    zoom: 12,
-    styles: [
-          {
-            featureType: "water",
-            stylers: [
-              { color: "#19a0d8" }
-            ]
-          },{
-            featureType: "road.highway",
-            elementType: "geometry.stroke",
-            stylers: [
-              { color: "#efe9e4" },
-              { lightness: -40 }
-            ]
-          },{
-            featureType: "transit.station",
-            stylers: [
-              { weight: 9 },
-              { hue: "#e85113" }
-            ]
-          },{
-            featureType: "road.highway",
-            elementType: "labels.icon",
-            stylers: [
-              { visibility: "off" }
-            ]
-          },{
-            featureType: "water",
-            elementType: "labels.text.fill",
-            stylers: [
-              { lightness: -100 }
-            ]
-          },{
-            featureType: "poi",
-            elementType: "geometry",
-            stylers: [
-              { visibility: "on" },
-              { color: "#f0e4d3" }
-            ]
-          },{
-            featureType: "road.highway",
-            elementType: "geometry.fill",
-            stylers: [
-              { color: "#efe9e4" },
-              { lightness: -25 }
-            ]
-          }
+    var mapOptions = {
+        "center": {
+            "lat": 33.4255104,
+            "lng": -111.9400054
+        },
+        zoom: 12,
+        styles: [
+            {
+                featureType: "water",
+                stylers: [
+                    {color: "#19a0d8"}
+                ]
+            }, {
+                featureType: "road.highway",
+                elementType: "geometry.stroke",
+                stylers: [
+                    {color: "#efe9e4"},
+                    {lightness: -40}
+                ]
+            }, {
+                featureType: "transit.station",
+                stylers: [
+                    {weight: 9},
+                    {hue: "#e85113"}
+                ]
+            }, {
+                featureType: "road.highway",
+                elementType: "labels.icon",
+                stylers: [
+                    {visibility: "off"}
+                ]
+            }, {
+                featureType: "water",
+                elementType: "labels.text.fill",
+                stylers: [
+                    {lightness: -100}
+                ]
+            }, {
+                featureType: "poi",
+                elementType: "geometry",
+                stylers: [
+                    {visibility: "on"},
+                    {color: "#f0e4d3"}
+                ]
+            }, {
+                featureType: "road.highway",
+                elementType: "geometry.fill",
+                stylers: [
+                    {color: "#efe9e4"},
+                    {lightness: -25}
+                ]
+            }
         ],
-    mapTypeId: google.maps.MapTypeId.ROADMAP,
-    mapTypeControl: false,
-    mapTypeControlOptions: {
-    style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
-    }
-  };
-  map = new google.maps.Map(document.getElementById("map"), mapOptions);
-  infowindow = new google.maps.InfoWindow({
-    maxWidth: 300,
-    content: ""
-  });
-  bounds = new google.maps.LatLngBounds();
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        mapTypeControl: false,
+        mapTypeControlOptions: {
+            style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
+        }
+    };
+    map = new google.maps.Map(document.getElementById("map"), mapOptions);
+    infowindow = new google.maps.InfoWindow({
+        maxWidth: 300,
+        content: ""
+    });
+    bounds = new google.maps.LatLngBounds();
 
   // Resize stuff...
-google.maps.event.addDomListener(window, "resize", function() {
-   var center = map.getCenter();
-   google.maps.event.trigger(map, "resize");
-      map.setCenter(center);
-});
+google.maps.event.addDomListener(window, "resize", function () {
+        var center = map.getCenter();
+        google.maps.event.trigger(map, "resize");
+        map.setCenter(center);
+    });
 
 
   // Close info-window when clicked else-where on the map
-  map.addListener("click", function(){
-    infowindow.close(infowindow);
-  });
+    map.addListener("click", function () {
+        infowindow.close(infowindow);
+    });
 
   // Bounce effect on marker
-  function toggleBounce(marker) {
-    if (marker.getAnimation() !== null) {
-      marker.setAnimation(null);
-    } else {
-      marker.setAnimation(google.maps.Animation.BOUNCE);
-      setTimeout(function() {
-        marker.setAnimation(null);
-      }, 700);
+    function toggleBounce(marker) {
+        if (marker.getAnimation() !== null) {
+            marker.setAnimation(null);
+        } else {
+            marker.setAnimation(google.maps.Animation.BOUNCE);
+            setTimeout(function () {
+                marker.setAnimation(null);
+            }, 700);
+        }
     }
-  }
 
  // Fetch infowindow content with foursquare data
-  function getContent(item) {
-    var contentString = "<h3>" + item.name +
-      "</h3><br><div style='width:200px;min-height:100px'><img src=" + '"' +
-      item.photoUrl + '"></div><div><a href="' + item.shortUrl +
-      '" target="_blank">Click here for Foursquare-info</a>';
-    var errorString = "Foursquare content unavailable.";
-    if (item.name.length > 0) {
-      return contentString;
-      } else {
-      return errorString;
-      }
-  }
+    function getContent(item) {
+        var contentString = "<h3>" + item.name +
+                "</h3><br><div style='width:200px;min-height:100px'><img src=" + '"' +
+                item.photoUrl + '"></div><div id="url"><a  href="' + item.shortUrl +
+                '" target="_blank">Click here for Foursquare-info</a></div>';
+        var errorString = "Foursquare has not been loaded";
+        if (item.name) {
+            return contentString;
+        } else {
+            return errorString;
+        }
+    }
 
   //Object for Places
-  var Place = function (data, map, id) {
-    var self = this;
-    this.title = ko.observable(data.title);
-    this.location = data.location;
-    this.marker = "";
-    this.markerId = id;
-    this.fs_id = data.fs_id;
-    this.shortUrl = "";
-    this.photoUrl = "";
-  };
+    var Place = function (data, map, id) {
+        var self = this;
+        this.title = ko.observable(data.title);
+        this.location = data.location;
+        this.marker = "";
+        this.markerId = id;
+        this.fs_id = data.fs_id;
+        this.shortUrl = "";
+        this.photoUrl = "";
+    };
 
 // Binding markers with the list-items
- function ViewModel() {
-    var self = this;
+    function ViewModel() {
+        var self = this;
 
     // Adding all places to form a list
-    this.spaceList = ko.observableArray();
-    initialSpaces.forEach(function(item){
-      self.spaceList.push(new Place(item));
-    });
+        this.spaceList = ko.observableArray();
+        initialSpaces.forEach(function (item) {
+            self.spaceList.push(new Place(item));
+        });
 
     // Create a marker for each item
-    this.spaceList().forEach(function(item) {
-      var marker = new google.maps.Marker({
-        map: map,
-        position: item.location,
-        icon: image,
-        animation: google.maps.Animation.DROP
-      });
-      item.marker = marker;
+        this.spaceList().forEach(function (item) {
+            var marker = new google.maps.Marker({
+                map: map,
+                position: item.location,
+                icon: image,
+                animation: google.maps.Animation.DROP
+            });
+            item.marker = marker;
       // Extends the boundaries of the map for each marker
-      bounds.extend(marker.position);
+            bounds.extend(marker.position);
       // eventlistener to display info-window and create bounce effect
-      marker.addListener("click", function(e) {
-        infowindow.setContent(getContent(item));
-        infowindow.open(map, marker);
-        toggleBounce(marker);
-    });
-  });
+            marker.addListener("click", function (e) {
+                infowindow.setContent(getContent(item));
+                infowindow.open(map, marker);
+                toggleBounce(marker);
+            });
+        });
 
     // Foursquare API request
-    self.getFoursquareData = ko.computed(function(){
-      self.spaceList().forEach(function(item) {
+        self.getFoursquareData = ko.computed(function () {
+            self.spaceList().forEach(function (item) {
 
         // Url building for each list item
-        var  venueId = item.fs_id + "/?";
-        var foursquareUrl = BaseUrl + venueId + fsClient_id + fsClient_secret + fsVersion;
+                var venueId = item.fs_id + "/?";
+                var foursquareUrl = BaseUrl + venueId + fsClient_id + fsClient_secret + fsVersion;
 
         // AJAX call to Foursquare
-        $.ajax({
-          type: "GET",
-          url: foursquareUrl,
-          dataType: "json",
-          cache: false,
-          success: function(data) {
-            var response = data.response ? data.response : "";
-            var venue = response.venue ? data.venue : "";
-                item.name = response.venue.name;
-                item.shortUrl = response.venue.shortUrl;
-                item.photoUrl = response.venue.bestPhoto.prefix + "height200" +
-                response.venue.bestPhoto.suffix;
-          }
+                $.ajax({
+                    type: "GET",
+                    url: foursquareUrl,
+                    dataType: "json",
+                    cache: false
+                }).done(function (data) {
+                    var response = data.response ? data.response : "";
+                    var venue = response.venue ? data.venue : "";
+                    item.name = response.venue.name;
+                    item.shortUrl = response.venue.shortUrl;
+                    item.photoUrl = response.venue.bestPhoto.prefix + "height200" +
+                            response.venue.bestPhoto.suffix;
+                }).error(function (e) {
+                    item.name = "this Item has not been loaded";
+                    item.photoUrl = "img/error.jpg";
+                    item.shortUrl = "file not found";
+            // $("#url").hide();
+                })
+
+            });
         });
-      });
-    });
+
 
     // call function upon clicking the item
     this.itemClick = function (item) {
-      var markerId = item.markerId;
-      google.maps.event.trigger(item.marker, "click");
-    };
+            var markerId = item.markerId;
+            google.maps.event.trigger(item.marker, "click");
+        };
 
     // Filter the list
-    self.filter = ko.observable("");
-    this.filteredSpaceList = ko.dependentObservable(function() {
-      var userinput = this.filter().toLowerCase();
-      if (!userinput) {
-      return ko.utils.arrayFilter(self.spaceList(), function(item) {
-        item.marker.setVisible(true);
-        return true;
-      });
-      } else {
-        return ko.utils.arrayFilter(this.spaceList(), function(item) {
-          if (item.name.toLowerCase().indexOf(userinput) >= 0) {
-          return true;
-          } else {
-            item.marker.setVisible(false);
-          return false;
-          }
-        });
-      }
-    }, this);
-  }
+        self.filter = ko.observable("");
+        this.filteredSpaceList = ko.dependentObservable(function () {
+            var userinput = this.filter().toLowerCase();
+            if (!userinput) {
+                return ko.utils.arrayFilter(self.spaceList(), function (item) {
+                    item.marker.setVisible(true);
+                    return true;
+                })
+            } else {
+                return ko.utils.arrayFilter(this.spaceList(), function (item) {
+                    if (item.name.toLowerCase().indexOf(userinput) >= 0) {
+                        item.marker.setVisible(true);
+                        return true;
+                    } else {
+                        item.marker.setVisible(false);
+                        return false;
+                    }
+                });
+            }
+        }, this);
+    }
 
  // Apply bindings to the view model
-ko.applyBindings(new ViewModel());
+     ko.applyBindings(new ViewModel());
 }
- var mapError = function() {
-
-    document.getElementById("map-error").style.display = "block";
-
-    document.getElementById("map-error").innerHTML = "Google map could not be loaded..please try again";
+var mapError = function () {
+    "use strict";
+    var errorMsg = "Google map could not be loaded..please try again";
+    $(".map-error").show();
+    return errorMsg;
 };
---
+++ js/data.js
@@ -0,0 +1,38 @@
+//List of Restaurants defined to appear on the page
+var initialSpaces = [
+          {
+          	"title": "Mika's Greek",
+           	"location": {"lat": 33.3495856 , "lng": -111.949818},
+           	"fs_id": "515f0b677ab473662460166d" 
+          },
+
+          {
+          	"title": "Olive Garden",
+          	"location": {"lat": 33.34965, "lng": -111.9550835},
+          	"fs_id": "4b5b5888f964a52068f628e3"
+          },
+
+          {
+          	"title": "Tzikii Pita Grill",
+          	"location": {"lat": 33.3501265 , "lng": -111.9637567 }, 
+          	"fs_id": "5151e5d9e4b03359ba73109f"
+          },
+
+          {
+          	"title": "Crêpe Bar",
+          	"location": {"lat": 33.3499291 , "lng": -111.9299471 },
+          	"fs_id": "4fcedee30cd6e871c8030eed"
+      	  },
+
+          {
+          	"title": "Garcia's Mexican Restaurant",
+          	"location": {"lat": 33.3832233 , "lng":-111.9644216 },
+          	"fs_id": "50d4f6ace4b028bbc68635c1"
+          },
+
+          {
+          	"title": "Opa Life Greek Cafe",
+          	"location": {"lat": 33.3777804 , "lng": -111.9360861 },
+          	"fs_id": "53c84bcd498e35e3e45faab5"
+     	  }
+        ];
